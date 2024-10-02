61d3ec8c3fbc47a8ad9d2d1be2de29bd17ab2b76
Bhushan Jadhav <143585131+Bhu3203@users.noreply.github.com>
2024-10-02 19:45:32 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,9 +1,30 @@
-package day5;
+package assignment6;
 
 public class Person {
+	private final String name = "Bhooolaaa";
+	private final int age = 24;
 
+	public String getName() {
+		return name;
+	}
 
-public void work() {
-	System.out.println("Person work");
-}
+	public int getAge() {
+		return age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+	public static void main(String[] args) {
+		Person person = new Person();
+//		person.set
+		System.out.println(person);
+		System.out.println("===============================");
+		Person person2 = new Person();
+//		person2.set
+		System.out.println(person2);
+	}
+
 }
